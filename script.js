c193fd1a540b3f5a41baae153db4e4b099855a78
Adenikemi <111865567+Adenikemi@users.noreply.github.com>
2023-11-06 12:53:13 +0100

    Finished Web Portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+function togglemenu(){
+    const menu = document.querySelector(".menu-links")
+    const icons = document.querySelector(".description-icon")
+    menu.classList.toggle("open")
+    icons.classList.toggle("open")
+}
